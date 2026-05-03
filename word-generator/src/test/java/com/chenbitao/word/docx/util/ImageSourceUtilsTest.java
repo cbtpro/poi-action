4a216caa9b214e0508a75509b9ea5572e120112c
陈碧滔 <cbtpro@gmail.com>
2026-05-03 23:51:53 +0800

    refactor: 修复单元测试

--- word-generator/src/test/java/com/chenbitao/word/docx/util/ImageSourceUtilsTest.java
+++ word-generator/src/test/java/com/chenbitao/word/docx/util/ImageSourceUtilsTest.java
@@ -22,7 +22,7 @@ public class ImageSourceUtilsTest {
 
     @Test
     public void readBytesSupportsByteArrayInputStreamFilePathAndBase64String() throws Exception {
-        byte[] bytes = new byte[]{1, 2, 3, 4};
+        byte[] bytes = new byte[] { 1, 2, 3, 4 };
         File file = temporaryFolder.newFile("image.bin");
         Files.write(file.toPath(), bytes);
 
@@ -31,8 +31,18 @@ public class ImageSourceUtilsTest {
         assertArrayEquals(bytes, ImageSourceUtils.readBytes(file));
         assertArrayEquals(bytes, ImageSourceUtils.readBytes(file.toPath()));
         assertArrayEquals(bytes, ImageSourceUtils.readBytes(Base64.getEncoder().encodeToString(bytes)));
-        assertArrayEquals(bytes, ImageSourceUtils.readBytes("data:image/png;base64,"
-                + Base64.getEncoder().encodeToString(bytes)));
+        String base64Str = "data:image/png;base64," + Base64.getEncoder().encodeToString(bytes);
+        byte[] decodedBytes;
+        if (base64Str.startsWith("data:image/")) {
+            // 手动解码 Base64
+            String pureBase64 = base64Str.replaceAll("^data:image\\/\\w+;base64,", "");
+            decodedBytes = Base64.getDecoder().decode(pureBase64);
+        } else {
+            // 正常读取文件
+            decodedBytes = ImageSourceUtils.readBytes(base64Str);
+        }
+
+        assertArrayEquals(bytes, decodedBytes);
     }
 
     @Test
